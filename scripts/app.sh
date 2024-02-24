dcaa45b63f251dcbb9fa48c06f50ea0740d6e4d6
Saikiran8 <kollojusaikiran@gmail.com>
2024-02-24 16:04:43 -0500

    assignment5

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "in app.sh"
-sudo mkdir /home/csye6225/webapp_main
+sudo mkdir /opt/webapp
 
-sudo mv /tmp/webapp.zip /home/csye6225/webapp_main
+sudo mv /tmp/webapp.zip /opt/webapp
 
 
-sudo unzip /home/csye6225/webapp_main/webapp.zip -d /home/csye6225/webapp_main
+sudo unzip /opt/webapp/webapp.zip -d /opt/webapp
 
-sudo rm /home/csye6225/webapp_main/webapp.zip
+sudo rm /opt/webapp/webapp.zip
 
 sudo python3 -m venv venv
 source venv/bin/activate 
@@ -15,11 +15,14 @@ source venv/bin/activate
 pwd
 
 ls
+
+cd venv 
+
 sudo python3 -m pip install --upgrade pip
 
 sudo pip3 install setuptools-rust bcrypt blinker certifi cffi charset-normalizer click colorama cryptography Flask Flask-Bcrypt Flask-HTTPAuth Flask-SQLAlchemy greenlet idna iniconfig itsdangerous Jinja2 MarkupSafe packaging pluggy pycparser PyJWT PyMySQL pytest python-dotenv regex requests SQLAlchemy typing_extensions urllib3 uuid Werkzeug
 
-sudo chown -R csye6225:csye6225 /home/csye6225/webapp_main/
+sudo chown -R csye6225:csye6225 /opt/webapp/
 
 sudo cp /tmp/webapp.service /etc/systemd/system/webapp.service
 

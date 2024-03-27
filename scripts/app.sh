6099be32518828825fc9afb464ae9a489653a476
Saikiran8 <kollojusaikiran@gmail.com>
2024-03-27 16:39:43 -0400

    ass7

--- scripts/app.sh
+++ scripts/app.sh
@@ -22,7 +22,7 @@ cd venv
 
 sudo python3 -m pip install --upgrade pip
 
-sudo pip3 install setuptools-rust bcrypt blinker certifi cffi charset-normalizer click colorama cryptography Flask Flask-Bcrypt Flask-HTTPAuth Flask-SQLAlchemy greenlet idna iniconfig itsdangerous Jinja2 MarkupSafe packaging pluggy pycparser PyJWT PyMySQL pytest python-dotenv regex requests SQLAlchemy typing_extensions urllib3 uuid Werkzeug python-json-logger
+sudo pip3 install setuptools-rust bcrypt blinker certifi cffi charset-normalizer click colorama cryptography Flask Flask-Bcrypt Flask-HTTPAuth Flask-SQLAlchemy greenlet idna iniconfig itsdangerous Jinja2 MarkupSafe packaging pluggy pycparser PyJWT PyMySQL pytest python-dotenv regex requests SQLAlchemy typing_extensions urllib3 uuid Werkzeug python-json-logger google-api-core google-auth google-cloud-pubsub googleapis-common-protos grpc-google-iam-v1 grpcio grpcio-status logging proto-plus protobuf pyasn1 pyasn1-modules rsa
 
 sudo cp /tmp/webapp.service /etc/systemd/system/webapp.service
 sudo cat /opt/webapp/config.yaml | sudo tee -a /etc/google-cloud-ops-agent/config.yaml > /dev/null
